96877bd0b9c7250119afcfc216515c0f6435721c
YAZEED THNIBAT <yazeedthnibat.19.6.1984@gmail.com>
2026-09-11 09:39:58 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,3 +4,10 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11750276
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
